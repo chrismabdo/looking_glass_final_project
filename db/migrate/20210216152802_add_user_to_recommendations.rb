de84e792af093a1fc88ad1bbd75d8b71697e0483
Chris Abdo <christophermabdo@icloud.com>
2021-02-16 15:29:47 +0000

    created recommendations table

---
+++ db/migrate/20210216152802_add_user_to_recommendations.rb
@@ -0,0 +1,5 @@
+class AddUserToRecommendations < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :recommendations, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_121604) do
+ActiveRecord::Schema.define(version: 2021_02_16_152802) do
+
+  create_table "recommendations", force: :cascade do |t|
+    t.string "note"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_recommendations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_02_16_121604) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recommendations", "users"
 end
