a04ea35b468641f3820ff972362f13eb45e6db32
ollienorman <oliver.norman1991@gmail.com>
2021-02-16 16:01:12 +0000

    sign in, out and up journey working

---
+++ app/javascript/components/ParentComponent.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class ParentComponent extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        Test
+      </React.Fragment>
+    );
+  }
+}
+
+export default ParentComponent
