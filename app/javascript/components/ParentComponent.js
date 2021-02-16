f797a0143aeac42d5ddbdd6949870e5a7730b0f6
ollienorman <oliver.norman1991@gmail.com>
2021-02-16 16:47:17 +0000

    passes current user data into React

--- app/javascript/components/ParentComponent.js
+++ app/javascript/components/ParentComponent.js
@@ -1,6 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
 class ParentComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log(this.props)
+  }
+
   render () {
     return (
       <React.Fragment>
