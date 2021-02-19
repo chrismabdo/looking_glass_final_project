21037cb6bfe020cf81c4fbdaf0e436a4b8df60b7
Ian Fleming <>
2021-02-19 11:51:29 +0000

    friend link

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 2021_02_18_114653) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "username", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
