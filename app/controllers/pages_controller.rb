950fcde947eae1bef3a9143ff5cb7ab22a3bbede
ollienorman <oliver.norman1991@gmail.com>
2021-02-24 17:56:35 +0000

    inital pass of movie data into react

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @recommendations = Recommendation.select("note, user_id, recommendations.id, title, release_date, overview, poster_path, api_id").joins("INNER JOIN movies ON recommendations.movie_id = movies.id")
+    @wishlists = Wishlist.select("note, user_id, wishlists.id, title, release_date, overview, poster_path, api_id").joins("INNER JOIN movies ON wishlists.movie_id = movies.id")
   end
 end
